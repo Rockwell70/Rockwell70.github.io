f2a40dfd78fbb0f86fc86a26759a7f929fd2db75
Rockwell70 <rockwellshabani@gmail.com>
2022-06-11 11:18:54 -0700

    added image

---
+++ resources/dom-functions.js
@@ -0,0 +1,14 @@
+const buttonElement = document.getElementById('secret-button');
+const imgElement = document.getElementById('secret-image');
+
+const toggleHiddenElement = (domElement) => {
+    if (domElement.style.display === 'none') {
+        domElement.style.display = 'block';
+    } else {
+        domElement.style.display = 'none';
+    }
+}
+
+buttonElement.addEventListener('click', () => {
+    toggleHiddenElement(imgElement);
+});
