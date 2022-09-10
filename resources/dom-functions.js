eb8897aa0cb393a544dc2197ab0d983dbf883536
Rockwell70 <rockwellshabani@gmail.com>
2022-09-10 16:26:48 -0700

    added color palette

--- resources/dom-functions.js
+++ resources/dom-functions.js
@@ -1,16 +1,36 @@
+$(document).ready(() => {
+  $("#secret-button").on("click", () => {
+    $("#secret-image").toggle("slow")
+      // .css({
+      //   display: "inline-flex",
+      //   height: "300px",
+      //   width: "400px",
+      //   boxShadow: "25px 25px 15px gray",
+      // });
+  });
+  $('.rayray').on('change', function () {
+      let day = new Date().getDay();
+      if (day === 6) {
+          $('.rayray').toggle('slow')
+      }
+    })
+
+
+});
+
 const buttonElement = document.getElementById('secret-button');
 const imgElement = document.getElementById('secret-image');
 
 let buttonText = buttonElement.textContent || buttonElement.innerText;
 
-const toggleHiddenElement = (domElement) => {
-  if (domElement.style.display === "none") {
-    domElement.style.display = "block";
-  } else {
-    domElement.style.display = "none";
-  }
-};
+// const toggleHiddenElement = (domElement) => {
+//   if (domElement.style.display === "none") {
+//     domElement.style.display = "block";
+//   } else {
+//     domElement.style.display = "none";
+//   }
+// };
 
-buttonElement.addEventListener("click", () => {
-  toggleHiddenElement(imgElement);
-});
+// buttonElement.addEventListener("click", () => {
+//   toggleHiddenElement(imgElement);
+// });
