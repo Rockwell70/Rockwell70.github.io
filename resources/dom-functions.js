4a5b66bdc680c54db2e900a5770f7bb31f767da8
Rockwell70 <jrshabani@outlook.com>
2024-06-07 16:24:41 -0700

    new zCK QUOTE

--- resources/dom-functions.js
+++ resources/dom-functions.js
@@ -57,7 +57,7 @@ $(document).ready(() => {
 
   $(".zacky").on("click", () => {
     $("#zack-caption").css({overflowWrap: "break-word"}).html(function () {
-      const num = Math.floor(Math.random() * 3);
+      const num = Math.floor(Math.random() * 4);
       switch (num) {
         case 0:
           return '<h3 id="zack-caption" style="color:#038C25; text-shadow: none;"><i>You look like you need a popper!</i>&#128138;</h3>';
@@ -65,6 +65,8 @@ $(document).ready(() => {
           return '<h3 id="zack-caption" style="color:#038C25; text-shadow: none;">&#128562;<i>That feels weird.</i>&#129312;</h3>';
         case 2:
           return '<h3 id="zack-caption" style="color:#038C25; text-shadow: none;">&#9939;<i>Wonder if this straight guy is enjoying this as much as me?.</i>&#129300;</h3>';
+        case 4:
+          return '<h3 id="zack-caption" style="color:#038C25; text-shadow: none;">&#9939;<i>Did I spray tan my nuts?.</i>&#129300;</h3>';
       }
     });
 
